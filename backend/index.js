3d235848efc12cecdd1fbee8ff16bc76ab252b3b
ravishankar0908 <ravishankar007cbi@gmail.com>
2025-08-21 13:45:57 +0530

    connection to mongoose is modified

--- backend/index.js
+++ backend/index.js
@@ -14,17 +14,18 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/jobs", jobRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 // database connection
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("Database is connected successfully...");
 };
 
-app.listen(process.env.PORT, () => {
-  connectDB();
-  console.log(`Server is running on port: ${process.env.PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port: ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err.message);
+  });
